e23f914a43e729bbc70e2413fc43ef0fd35b60b2
Drako <Drako@DESKTOP-GAQCQC9.fritz.box>
2020-11-19 10:03:55 +0100

    PRIMO PASSO

---
+++ GITHUB/src/provagit/Persona.java
@@ -0,0 +1,73 @@
+package provagit;
+
+public class Persona {
+	
+	private String nome;
+	private String cognome;
+	
+	
+	
+	
+	
+	
+	
+	public Persona(String nome, String cognome) {
+		super();
+		setNome(nome);
+		setCognome(cognome);
+	}
+
+
+
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+
+
+
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Persona [nome=" + nome + ", cognome=" + cognome + "]";
+	}
+	
+	
+
+}
